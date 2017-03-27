fa78c6610c06114850a2fe9abd1b60086a50a8fd
Annie Ton-Nu <annieqton@gmail.com>
2017-03-27 12:20:49 -0700

    lecture notes

---
+++ Picsfeed/Picsfeed/CloudKit.swift
@@ -0,0 +1,23 @@
+//
+//  CloudKit.swift
+//  Picsfeed
+//
+//  Created by Annie Ton-Nu on 3/27/17.
+//  Copyright © 2017 Annie Ton-Nu. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class CloudKit {
+    
+    static let shared = CloudKit()
+    
+    let container = CKContainer.default()
+    
+    var privateDatabase : CKDatabase {
+        return container.privateCloudDatabase
+    }
+    
+    
+}
