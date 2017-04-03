793b6ab8dd67ab24c2264197166580704b3e9043
Annie Ton-Nu <annieqton@gmail.com>
2017-04-02 22:57:21 -0700

    deleted some old codes

--- Picsfeed/Picsfeed/FilterCell.swift
+++ Picsfeed/Picsfeed/FilterCell.swift
@@ -12,24 +12,18 @@ class FilterCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
     
-//    @IBOutlet weak var filterLabel: UILabel!
+
+    @IBOutlet weak var filterLabel: UILabel!
 
     
     override func prepareForReuse() {
         super.prepareForReuse()
         self.imageView.image = nil
-//        self.filterLabel.text = nil
+
         
     }
     
-//    func makeFilterLabel(name: String) -> String {
-//        
-//        for index in filterNames {
-//         var index = 0
-//            filterLabel.text = filterNames[index]
-//        }
-//        
-//    }
+
     
     
 }
